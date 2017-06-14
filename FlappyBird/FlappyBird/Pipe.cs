90a4d05365de3abd451152d9d75b8af65775c8e0
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-14 19:52:59 +0200

    Step 3

---
+++ FlappyBird/FlappyBird/Pipe.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlappyBird
+{
+    class Pipe : SpriteGameObject
+    {
+        Vector2 StartPosition;
+
+        public Pipe() : base("spr_pipe")
+        {           
+            this.velocity = new Vector2(-300, 0);
+            this.origin = this.Center;
+            StartPosition = new Vector2(700, FlappyBird.Random.Next(201, 400));
+            this.Position = StartPosition;
+        }
+    }
+}
--- FlappyBird/FlappyBird/PlayingState.cs
+++ FlappyBird/FlappyBird/PlayingState.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace FlappyBird
 {
     class PlayingState : GameObjectList
     {
         Bird bird;
+        GameObjectList pipes;
+        int frameCounter = 0;
 
         public PlayingState()
         {
             bird = new Bird();
+            pipes = new GameObjectList();
 
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(bird);
+            this.Add(pipes);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            frameCounter++;
+
+            if(frameCounter > 100)
+            {
+                this.pipes.Add(new Pipe());
+                frameCounter = 0;
+            }
         }
     }
 }
