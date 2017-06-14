523dd1b3666fdd8a765901b504289ad72d3cb58d
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-14 19:14:55 +0200

    Step 2.1

---
+++ FlappyBird/FlappyBird/Bird.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlappyBird
+{
+    class Bird : RotatingSpriteGameObject
+    {
+        public Bird() : base("spr_bird")
+        {
+            this.origin = this.Center;
+        }
+    }
+}
--- FlappyBird/FlappyBird/PlayingState.cs
+++ FlappyBird/FlappyBird/PlayingState.cs
@@ -7,9 +7,14 @@ namespace FlappyBird
 {
     class PlayingState : GameObjectList
     {
+        Bird bird;
+
         public PlayingState()
         {
+            bird = new Bird();
+
             this.Add(new SpriteGameObject("spr_background"));
+            this.Add(bird);
         }
     }
 }
