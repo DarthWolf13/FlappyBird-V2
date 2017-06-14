90a939ea13f1e7d71f50ca50da6e5924beac2021
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-14 19:20:57 +0200

    Step 2.2+2.3

--- FlappyBird/FlappyBird/Bird.cs
+++ FlappyBird/FlappyBird/Bird.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +8,21 @@ namespace FlappyBird
 {
     class Bird : RotatingSpriteGameObject
     {
+        Vector2 StartPosition;
+
         public Bird() : base("spr_bird")
         {
             this.origin = this.Center;
+            StartPosition = new Vector2(FlappyBird.Screen.X / 2, FlappyBird.Screen.Y / 2);
+
+            this.Reset();
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            this.Position = StartPosition;
         }
     }
 }
